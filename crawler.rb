e586ac174bcff019b6a062aef6deba2e14323fe7
Tatsuki Sugiura <sugi@nemui.org>
2013-04-17 02:15:29 +0900

    Change class structure.

--- crawler.rb
+++ crawler.rb
@@ -3,60 +3,69 @@ require 'eventmachine'
 require 'em/pool'
 require 'em-http-request'
 
-class RssFetcher
-  class CategolizedPool < EM::Pool
-    def initialize(category_resource_size, category_reuse_wait = 0)
-      super()
-      @category_reuse_wait = category_reuse_wait
-      @category_resource_size = category_resource_size
-      @category_queue = Hash.new {|h, k|
-        q = EM::Queue.new
-        category_resource_size.times { q.push nil }
-        h[k] = q
-      }
-    end
-    
-    def perform(category, *a, &b)
-      @category_queue[category].pop do |c|
-        work = EM::Callback(*a, &b)
-        super {
-          rq = proc {
-            if @category_reuse_wait > 0
-              EM.add_timer(@category_reuse_wait) {
-                @category_queue[category].push c
-              }
-            else
-              @category_queue[category].push c
-            end
-          }
-          d = work.call
-          d.callback(&rq)
-          d.errback(&rq)
-          d
+module EventMachine
+  class HttpFetcher
+    class RequestPool < EM::Pool
+      def initialize(host_resource_size, host_reuse_wait = 0)
+        super()
+        @host_reuse_wait = host_reuse_wait
+        @host_resource_size = host_resource_size
+        @host_queue = Hash.new {|h, k|
+          q = EM::Queue.new
+          host_resource_size.times { q.push nil }
+          h[k] = q
         }
       end
+      
+      def perform(host, *a, &b)
+        @host_queue[host].pop do |c|
+          work = EM::Callback(*a, &b)
+          super {
+            rq = proc {
+              if @host_reuse_wait > 0
+                EM.add_timer(@host_reuse_wait) {
+                  @host_queue[host].push c
+                }
+              else
+                @host_queue[host].push c
+              end
+            }
+            d = work.call
+            d.callback(&rq)
+            d.errback(&rq)
+            d
+          }
+        end
+      end
     end
-  end
 
-  def initialize
-    @concurrency      = 40
-    @host_concurrency = 2
-    @request_pool     = CategolizedPool.new(@host_concurrency, 0.3)
-    @concurrency.times {|i| @request_pool.add i}
-  end
+    def initialize(opts = {})
+      @concurrency       = opts[:concurrency]       || 40
+      @host_concurrency  = opts[:host_concurrency]  || 2
+      @host_request_wait = opts[:host_request_wait] || 0.3
+      @request_pool      = nil
+    end
 
-  def request(*args)
-    url = args.first
-    host = url.sub(%r{^(http://[^/]+).*$}, "\\1")
-    @request_pool.perform(host) do
-      req = EM::HttpRequest.new(url).get *args[1..-1]
-      req.callback do
-        p [:success, url, req.response.size]
+    def request_pool
+      @request_pool and return @request_pool
+      @request_pool = RequestPool.new(@host_concurrency, @host_request_wait)
+      @concurrency.times {|i| @request_pool.add i}
+      @request_pool
+    end
+
+    def request(*args, &callback)
+      url = args.first
+      host = url.sub(%r{^(http://[^/]+).*$}, "\\1")
+      request_pool.perform(host) do
+        req = EM::HttpRequest.new(url).get *args[1..-1]
+        req.callback do
+          p [:success, url, req.response.size]
+        end
+        req.errback do
+          p [:err, url, req.response.size]
+        end
+        req
       end
-      req.errback do
-        p [:err, url, req.response.size]
-      end
-      req
     end
   end
 end
@@ -65,7 +74,7 @@ end
 if __FILE__ == $0
   trap(:INT) { EM.stop }
   EM.run do
-    r = RssFetcher.new
+    r = EM::HttpFetcher.new
     ARGF.each { |line|
       line.chomp!
       line or next
